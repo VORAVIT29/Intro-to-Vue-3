8c68ccef6d0195863f7b2d08f8db6f2473d73641
VORAVIT29 <6206021610153@fitm.kmutnb.ac.th>
2021-04-07 17:01:28 +0700

    update

--- components/ProductDetails.js
+++ components/ProductDetails.js
@@ -1,17 +1,17 @@
 // solution
 app.component('product-details', {
-  props: {
-    details: {
-      type: Array,
-      required: true
-    }
-  },
-  template:
-  /*html*/
-  `
+        props: {
+            details: {
+                type: Array,
+                required: true
+            }
+        },
+        template:
+        /*html*/
+            `
   <ul>
     <li v-for="detail in details">{{ detail }}</li>
   </ul>
   `
-})
-// solution
+    })
+    // solution
